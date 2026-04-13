fde57a030fe494156ae929fde8beb84ffbf141cb
Lunar Lumos <ariful242-16-025@diu.edu.bd>
2026-04-13 17:10:35 +0600

    Create 3.sh

---
+++ 3.sh
@@ -0,0 +1,12 @@
+echo -n "Enter the first number :"
+read num1
+
+echo -n "Enter the second number :"
+read num2
+
+while [ $num2 -ne 0 ]; do
+    temp=$num2
+    num2=$((num1 % num2))
+    num1=$temp
+done
+echo "GCD of the two numbers is $num1"
