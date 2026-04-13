a6cdf2055437e5380b97093ee9a1bc3ece80a3f3
Lunar Lumos <ariful242-16-025@diu.edu.bd>
2026-04-13 17:47:32 +0600

    Create 6.sh

---
+++ 6.sh
@@ -0,0 +1,18 @@
+
+echo -n "Enter a number :"
+read num
+
+org=$num
+sum=0
+
+while [ $num -gt 0 ]; do
+    digit=$((num % 10))
+    sum=$((sum + digit * digit * digit))
+    num=$((num / 10))
+done
+
+if [ $sum -eq $org ]; then
+    echo " The number $org is an Armstrong Number."
+else
+    echo " The number $org is not an Armstrong Number."
+fi
