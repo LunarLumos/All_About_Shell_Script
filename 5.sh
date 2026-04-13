ac07cbf6d66aa3826d77dda6f145a68a6c12b266
Lunar Lumos <ariful242-16-025@diu.edu.bd>
2026-04-13 17:40:42 +0600

    Create 5.sh

---
+++ 5.sh
@@ -0,0 +1,27 @@
+
+echo -n "Enter starting number :"
+read start
+
+echo -n "Enter ending number :"
+read end
+
+echo "prime numbers between $start and $end are :"
+
+for ((i=start; i<=end; i++)); do
+    if [ "$i" -lt 2 ]; then
+        continue
+    fi
+
+    num=$i
+    count=0
+
+    for ((j=1; j<=num; j++)); do
+        if [ $((num % j)) -eq 0 ]; then
+            count=$((count + 1))
+        fi
+    done
+
+    if [ $count -eq 2 ]; then
+        echo "$num"
+    fi
+done
