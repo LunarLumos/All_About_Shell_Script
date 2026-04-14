569c9edf5e07425086628f97fccffd6c1c1eeca1
Lunar Lumos <ariful242-16-025@diu.edu.bd>
2026-04-14 11:51:31 +0600

    Create 8.sh

---
+++ 8.sh
@@ -0,0 +1,17 @@
+
+echo -n "Enter a number :"
+read num
+
+sum=0
+
+for ((i=1;i<num;i++)); do
+    if [ $((num % i )) -eq 0 ]; then
+        sum=$((sum + i))
+    fi
+done
+
+if [ $sum -eq $num ]; then
+    echo "$num is a perfect number"
+else
+    echo "$num is not a perfect number"
+fi
