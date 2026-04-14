114ddfa15a036224140ce33e671f468a5832f0ff
Lunar Lumos <ariful242-16-025@diu.edu.bd>
2026-04-14 12:47:01 +0600

    Create 18.sh

---
+++ 18.sh
@@ -0,0 +1,20 @@
+
+echo -n "Enter numbers (space separated) :"
+read -a num_array
+
+max=${num_array[0]}
+min=${num_array[0]}
+
+for num in "${num_array[@]}"; do
+    if [ $num -gt $max ]; then
+        max=$num
+    fi
+
+    if [ $num -lt $min ]; then
+        min=$num
+    fi
+done
+
+
+echo "Maximum number is: $max"
+echo "Minimum number is: $min"
