2ceed26dc965ae089a977c29cc24f2ce3ff5dee5
Lunar Lumos <ariful242-16-025@diu.edu.bd>
2026-04-17 00:43:16 +0600

    Add script to sum array elements
    This script prompts the user for a number of elements, reads those elements into an array, and calculates their sum.

---
+++ 82.sh
@@ -0,0 +1,16 @@
+# Sum of Array Elements
+#!/bin/bash
+
+echo "Enter how many numbers"
+read n
+
+sum=0
+
+echo "Enter numbers:"
+for ((i=0;i<n;i++))
+do
+    read arr[$i]
+    sum=$((sum + arr[$i]))
+done
+
+echo "$sum"
